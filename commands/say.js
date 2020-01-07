82d27fce0ca9469e036b2f8b5508a65fe606bb41
RealPortalPlayer <snodgrass.pop@gmail.com>
2020-01-07 18:09:01 -0500

    Add files via upload

---
+++ commands/say.js
@@ -0,0 +1,16 @@
+const {success} = require("../settings.json")
+
+module.exports.run = async (bot, msg, args, commands) => {
+    msg.channel.send(args.join(" "))
+}
+
+module.exports.help = {
+    name: "say",
+    description: "Says the message.",
+    arguments: "<message>",
+    supportOnly: false,
+    staffOnly: false,
+    supportManagersOnly: false,
+    ownerOnly: false,
+    argumentsRequired: true
+}
